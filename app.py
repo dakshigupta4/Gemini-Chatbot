c2b1953389fdce0c1a76eae6bde51e9dbaad4766
Dakshi Gupta <71374300+dakshigupta4@users.noreply.github.com>
2024-11-08 10:59:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import streamlit as st
 import os
 import google.generativeai as genai
 ####
+####
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 model=genai.GenerativeModel("gemini-pro")
 def get_gemini_response(question):
